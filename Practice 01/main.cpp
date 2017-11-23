3e44cd72c1f22c5e1150159440b50b036c1ca709
amitushar <123.ki.tushar@gmail.com>
2017-11-23 22:27:05 +0600

    my first work

---
+++ Practice 01/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int inputArray[500],n,i;
+
+        cout<<"enter number of elements: ";
+        cin>>n;
+    for(i=0;i<n;i++){
+        cin>>inputArray[i];
+    }
+
+    cout<<"ArrayElements: ";
+
+    for(i=0;i<n;i++){
+        cout<<inputArray[i]<<" ";
+
+    }
+
+
+
+    return 0;
+
+}
